3ba5b262b3514a1c5c92b18b68fcfd0998016a61
Carter Roeser <carter@cdgtech.one>
2019-07-18 09:39:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
 
 	vList r1;
 
+<<<<<<< Updated upstream
 	// Sphere Only
 	 NewItem(r1, &Sphere(Vector3D(5, 3, 0), 1, new Lambertian(Vector3D(0.51, 0.7, 1.0))));
 
@@ -15,6 +16,15 @@ int main() {
 
 	SDim a = { 200, 100 };
 	RayTracer(a, 10, Camera(a, Vector3D(20, 3, 0)), r1, "RayTracer");
+=======
+	//NewItem(r1, &Sphere(Vector3D(3,0,0), 1, new Metal(Vector3D(1,0,0), 1)));
+	//NewItem(r1, &Box(Vector3D(-1), Vector3D(1), new Lambertian(Vector3D(0, 0, 0))));
+	NewItem(r1, &Sphere(Vector3D(0), 1, new Lambertian(Vector3D(0.51, 0.7, 1.0))).BBox(r1, new Lambertian(Vector3D(1, 0, 0))));
+
+	SDim a = { 100, 50 };
+	RayTracer(a, 10, Camera(a, Vector3D(5, 3, 0)), r1, "ray_tracer");
+
+>>>>>>> Stashed changes
 
 	return 0;
 }
